372b8d312bf03f76d7790f7dd11b102061873e4f
Hong Tan <h_tan@ucsb.edu>
2023-04-05 00:14:39 -0700

    able to put captions on mp4 file with moviepy

--- speech-to-text/whisper_timeframe.py
+++ speech-to-text/whisper_timeframe.py
@@ -2,12 +2,12 @@ import stable_whisper
 
 model = stable_whisper.load_model('base')
 # this modified model run just like the original model but accepts additional arguments
-result = model.transcribe('/Users/hongtan/Desktop/DSClub_Project/speech-to-text/audio-test-files/insane.wav')
+result = model.transcribe("/Users/hongtan/Desktop/DSClub_Project/speech-to-text/audio-test-files/Friends Joey's Bad Birthday Gift.mp4", fp16=False)
 
 result.to_srt_vtt('audio.srt')
 result.to_ass('audio.ass')
 # word_level=False : use only segment timestamps (i.e without the green highlight)
 # segment_level=False : use only word timestamps
 
-result.save_as_json('audio.json')
+# result.save_as_json('audio.json')
 # save inference result for later processing
