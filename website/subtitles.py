22b76969f15b7c9d8bddf1e0731f4a32f316bd9f
Scott Weston <scottricardo@ucsb.edu>
2023-04-05 17:15:06 -0700

    removed clutter

--- website/subtitles.py
+++ website/subtitles.py
@@ -3,14 +3,22 @@
 from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
 import whisper_timestamped as whisper
+import numpy as np
+import pandas as pd
+import json
+
+#constants
+#CHARS_PER_SCREEN = 25 #numbers of chars that can confortbaly fit on a screen
 
 #load the video
-video = VideoFileClip("./movie-clips/mclovin.mp4").subclip(0,10)
+video = VideoFileClip("./movie-clips/bee.mp4").subclip(47,57)
+#duration = video.duration
 
 #load the model
 model = whisper.load_model("tiny")
-result = model.transcribe(video)
-print(result["text"])
+result = model.transcribe('./movie-clips/mclovin.mp4')
+print(json.dumps(result, indent = 2, ensure_ascii = False))
+
 
 
 generator = lambda txt: TextClip(txt, font='Arial', fontsize=24, color='white')
