1bda93d9dbbf50d94b91c111ed90e78a09bb3a94
Hong Tan <h_tan@ucsb.edu>
2023-04-09 15:32:49 -0700

    Finished captioning at push of button

--- website/sentsub/fileupload/views.py
+++ website/sentsub/fileupload/views.py
@@ -15,40 +15,41 @@ def showvideo(request):
     if form.is_valid():
         form.save()
 
-    colors = {'fear': '#0000ff', 'joy': '#000000', 'anger':'#ff0000', 'sadness':'#00ff00'}
-    classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion')
+    if request.method == 'POST' and 'upload' in request.POST:
+        colors = {'fear': '#0000ff', 'joy': '#000000', 'anger':'#ff0000', 'sadness':'#00ff00'}
+        classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion')
 
-    # Make sure you are in DS_Project directory when running
-    srt_file = '/Users/hongtan/Desktop/sentimentsub/audio.srt'
-    mp4_file = './website/sentsub/media/videos/Friends_Joeys_Bad_Birthday_Gift.mp4'
+        # Make sure you are in DS_Project directory when running
+        srt_file = '/Users/hongtan/Desktop/sentimentsub/website/sentsub/audio.srt'
+        mp4_file = '/Users/hongtan/Desktop/sentimentsub/website/sentsub/media/videos/Friends_Joeys_Bad_Birthday_Gift.mp4'
 
-    # os.system(f'stable-ts {mp4_file} -o {output_file} --word_level False --fp16 False -y')
-    model = stable_whisper.load_model('base')
-    result = model.transcribe(mp4_file, fp16=False)
-    result.to_srt_vtt('audio.srt', word_level=False)
+        # os.system(f'stable-ts {mp4_file} -o {output_file} --word_level False --fp16 False -y')
+        model = stable_whisper.load_model('base')
+        result = model.transcribe(mp4_file, fp16=False)
+        result.to_srt_vtt('audio.srt', word_level=False)
 
-    # Read and classify each line of dialogue
-    with open(srt_file, 'r') as f:
-        lines = f.readlines()
-        n = len(lines)
-        for i in range(2,n,4):
-            label = classifier(lines[i])[0]['label']
-            color = colors[label]
-            new_line = f'<font color="{color}">' + lines[i] + '</font>\n'
-            lines[i] = new_line
+        # Read and classify each line of dialogue
+        with open(srt_file, 'r') as f:
+            lines = f.readlines()
+            n = len(lines)
+            for i in range(2,n,4):
+                label = classifier(lines[i])[0]['label']
+                color = colors[label]
+                new_line = f'<font color="{color}">' + lines[i] + '</font>\n'
+                lines[i] = new_line
 
-    with open(srt_file, 'w') as f:
-        f.writelines(lines)
+        with open(srt_file, 'w') as f:
+            f.writelines(lines)
 
-    # command = f'ffmpeg -i {mp4_file} -vf subtitles={srt_file} output_srt.mp4 -y'
-    # os.system(command)
+        # command = f'ffmpeg -i {mp4_file} -vf subtitles={srt_file} output_srt.mp4 -y'
+        # os.system(command)
 
-    ff = FFmpeg(
-        inputs={'/Users/hongtan/Desktop/sentimentsub/website/sentsub/media/videos/Friends_Joeys_Bad_Birthday_Gift.mp4': None},
-        outputs={'output_srt.mp4': f'-vf subtitles={srt_file} -y'}
-    )
+        ff = FFmpeg(
+            inputs={f'{mp4_file}': None},
+            outputs={'output_srt.mp4': f'-vf subtitles={srt_file} -y'}
+        )
 
-    ff.run()
+        ff.run()
 
     context= {'videofile': videofile, 'form': form,}
 
